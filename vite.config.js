ed9e19d2862051383b19030baa92cab4ad027c8e
Suegathh <suzzainnegathu@gmail.com>
2024-11-24 16:18:16 +0300

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Codekazi",
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  base: '/Codekazi/', // Update this based on your deployment requirements
+});
